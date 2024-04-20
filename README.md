7ca2c7b1c2386c8a5e6b821542ecb9f12a3e1155
Francis Lahoud <francis.lahoud@proton.me>
2024-04-19 21:28:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Streamlit Accounting Visualization
+
+## For developpers
+
+1. Set up your environment: Follow [Streamlit Documentation](https://docs.streamlit.io/get-started/installation/command-line)
---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import polars as pl
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read(
+    worksheet="Transactions",
+    ttl="10m",
+)
+df['Debit'] = df['Debit'].map(lambda x: f"${x:.00f}")
+
+# Group by 'Account' using Pandas.
+grouped_df = df.groupby('Account')['Debit'].sum().reset_index()
+
+# Display bar chart
+st.bar_chart(grouped_df.set_index('Account'))
